dc5394fd8aac6e7c40c5becedac28834052ef283
commiegoat <iolson91@gmail.com>
2017-06-08 19:08:55 -0500

    fixed browser inconsistencies

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 This is a simple drawing program (a la etch-a-sketch) using HTML, CSS, Javascript, and Jquery.
 
 Made as part of the Odin Project.
+
+Updates:
+
+Learned a lot doing this project.  I think I'm going to call it "good enough" for now, though tons of improvements could be made.
+
+Biggest (and most annoying thing) I learned was to check how your code renders in different browsers - I encountered a bug in chrome where the inline-block elements of my board were flowing out the bottom of my board element (somehow, the boxes were rendering taller than they should have.  Rounding error in chrome?).  Firefox looked fine, so I didn't notice this until my final commits.  Using Float and percentage-based sizing on my divs instead seems to have solved this problem.
+
+Ian Olson
+6/2017
--- script.js
+++ script.js
@@ -1,41 +1,42 @@
 $(document).ready(function() {
-  
-  generate(16);
+	generate(16);
 
-  $("#btn").on("click", function() {
-    $("#shaker").effect("shake", {direction: "up", times: 3, distance: 17}, 600);
-    $(".fabuloso").remove();
-    $("#board").find('br').remove();
-    window.setTimeout(numRows, 600);
-  })
-  
-  function numRows() {
-  	var rows = window.prompt("Enter a number under 64", 16);
-  	generate(rows);
-  }
+	$("#btn").on("click", function() {
+		$("#shaker").effect("shake", {direction: "up", times: 3, distance: 17}, 600);
+		$(".fabuloso").remove();
+		$("#board").find('br').remove();
+		window.setTimeout(numRows, 600);
+	})
 
-  function generate(x) {
-  	var $size = ($("#board").width()/x);
-  	if ($.isNumeric(x) && x<=64) {
-	  	for (var i = 0; i < x; i++) {     
-	      
-	      var $div = $("#board").append( $("<div/>").addClass('fabuloso').css({"height": $size, "width": $size}));     
-	        for (var j = 1; j < x; j++) {
-	          $("#board").append( $("<div/>").addClass('fabuloso').css({"height": $size, "width": $size}));
-	        }
-	        $("#board").append("<br/>");
-	    }
-  	} else {
-  		alert(x + " is not a number or is too big!")
-  		numRows();
+	function numRows() {
+		var rows = window.prompt("Enter a number 1-64", 16);
+		generate(rows);
   	}
-      fabulize();
-    }
 
-  function fabulize() {
-    $(".fabuloso").on("mouseover", function() {
-      var $color = $("#penColor").val();
-      $(this).css("background-color", $color);
-    });
-  };
+  	function generate(x) {
+		var calc = (1/x)*100;
+		size = calc + "%";
+		if (x > 0 && x <=64) {
+			for (var i = 0; i < x; i++) {
+				var $div = $("#board").append( $("<div/>").addClass('fabuloso').css({"height": size, "width": size}));     
+				for (var j = 1; j < x; j++) {
+					$("#board").append( $("<div/>").addClass('fabuloso').css({"height": size, "width": size}));
+				}
+			}
+		} else if (x<0 || x>64 || x===0){
+			alert(x + " is not a valid number.  Enter a number 1-64")
+			numRows();
+		} else {
+			alert(x + " is not a number.  Enter a number 1-64")
+			numRows();
+		}
+		fabulize();
+	}
+
+  	function fabulize() {
+		$(".fabuloso").on("mouseover", function() {
+			var $color = $("#penColor").val();
+			$(this).css("background-color", $color);
+		});
+	};
 });
